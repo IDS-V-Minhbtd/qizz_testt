170001e8462f79c423e00dac99bdfcab6fdba672
IDS-V-Minhbtd <minhbtd@vn.ids.jp>
2025-04-24 13:21:48 +0700

    sử lí conflic

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\QuizService;
 
 class QuizController extends Controller
 {
-    //
+    protected $service;
+
+    public function __construct(QuizService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function index()
+    {
+        // Fetch all quizzes and return the view
+        $quizzes = $this->service->getAll();
+        return view('quizzes.index', compact('quizzes'));
+    }
 }
